3490267a3eaf74531865b1e957d5ffef050fdab6
Irek <general157@mail.ru>
2022-12-13 20:53:57 +0300

    floodFill is added

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,20 @@
 public class Solution {
     private int maxSize = 0;
     private int size = 0;
+
     public int maxAreaOfIsland(int[][] grid) {
-         for (int i = 0; i < grid.length; i++) {
-             for (int j = 0; j < grid[i].length; j++) {
-                 if(grid[i][j] == 1) {
-                     dfs(i, j, grid);
-                     if (size > maxSize) {
-                         maxSize = size;
-                     }
-                     size = 0;
-                 }
-             }
-         }
-         return maxSize;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                    if (size > maxSize) {
+                        maxSize = size;
+                    }
+                    size = 0;
+                }
+            }
+        }
+        return maxSize;
     }
 
     private void dfs(int i, int j, int[][] grid) {
@@ -26,5 +27,25 @@ public class Solution {
         dfs(i + 1, j, grid);
         dfs(i, j - 1, grid);
         dfs(i, j + 1, grid);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int currentColor = image[sr][sc];
+        findPixel(image, sr, sc, currentColor, color);
+        return image;
+    }
+
+    private void findPixel(int[][] image, int row, int col, int curColor, int color) {
+        if (row < 0 || row >= image.length
+                || col < 0 || col >= image[row].length
+                || image[row][col] != curColor
+                || color == curColor) {
+            return;
+        }
+        image[row][col] = color;
+        findPixel(image, row - 1, col, curColor, color);
+        findPixel(image, row + 1, col, curColor, color);
+        findPixel(image, row, col - 1, curColor, color);
+        findPixel(image, row, col + 1, curColor, color);
     }
 }
